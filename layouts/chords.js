6b5dde0927edfe821403443b79ef4482b5e77b80
Richard Hindes <rmhindes1@sheffield.ac.uk>
2019-07-11 17:30:05 +0100

    Added a page for diatonic chords, pulling data from a JS dictionary

---
+++ layouts/chords.js
@@ -0,0 +1,64 @@
+$scope.DiatonicMajorChords = [
+        {
+            "key": "C",
+            "chords": ["C", "Dm", "Em", "F", "G", "Am", "Bdim"]
+        },
+        {
+            "key": "G",
+            "chords": ["G", "Am", "Bm", "C", "D", "Em", "F#dim"]
+        },
+        {
+            "key": "D",
+            "chords": ["D", "Em", "F#m", "G", "A", "Bm", "C#dim"]
+        },
+        {
+            "key": "A",
+            "chords": ["A", "Bm", "C#m", "D", "E", "F#m", "G#dim"]
+        },
+        {
+            "key": "E",
+            "chords": ["E", "F#m", "G#m", "A", "B", "C#m", "D#dim"]
+        },
+        {
+            "key": "B",
+            "chords": ["B", "C#m", "D#m", "E", "F#", "G#m", "A#dim"]
+        },
+        {
+            "key": "F#",
+            "chords": ["F#", "G#m", "A#m", "B", "C#", "D#m", "E#dim"]
+        },
+        {
+            "key": "C#",
+            "chords": ["C#", "D#m", "E#m", "F#", "G#", "A#m", "B#dim"]
+        },
+        {
+            "key": "F",
+            "chords": ["F", "Gm", "Am", "B♭", "C", "D", "Edim"]
+        },
+        {
+            "key": "B♭",
+            "chords": ["B♭", "Cm", "Dm", "E♭", "F", "G", "Adim"]
+        },
+        {
+            "key": "E♭",
+            "chords": ["E♭", "Fm", "Gm", "A♭", "B♭", "Cm", "Ddim"]
+        },
+        {
+            "key": "A♭",
+            "chords": ["A♭", "B♭m", "Cm", "D♭", "E♭", "Fm", "Gdim"]
+        },
+        {
+            "key": "D♭",
+            "chords": ["D♭", "E♭m", "Fm", "G♭", "A♭", "B♭m", "Cdim"]
+        },
+        {
+            "key": "G♭",
+            "chords": ["G♭", "A♭m", "B♭m", "C♭", "D♭", "E♭m", "Fdim"]
+        },
+        {
+            "key": "C♭",
+            "chords": ["C♭", "D♭m", "E♭m", "F♭", "G♭", "A♭m", "Bdim"]
+        },
+
+
+    ];
