42fed29dcba0921295d8d31ceeffddbb314c7b7f
Richard Hindes <rmhindes1@sheffield.ac.uk>
2019-07-14 20:14:54 +0100

    Chord progression page now supports all major keys

--- scripts/progressions.js
+++ scripts/progressions.js
@@ -1,5 +1,30 @@
-var c = ["C", "D", "E", "F", "G", "A", "B"];
-var cMajor = getChordsFromScale(c);
+/* for each key, populate the dropdown list with an option to select that key */
+document.addEventListener('DOMContentLoaded', function() {
+  var dropdown = document.getElementById("dropdown");
+  for (var i = 0; i < majorScales.length; i++) {
+    var key = majorScales[i];
+    var el = document.createElement("button");
+    el.textContent = (key.value)[0] + " major";
+    el.value = key.value;
+    el.className = "dropdown-item";
+    dropdown.appendChild(el);
+    }
+
+    //add onClick events for each button to populate table with chord progressions
+    $('.dropdown-menu button').click(function(){
+        //get value from dropdown
+        $("#tbody").empty();
+        var tbody = document.getElementById('tbody');
+        var asArray = (this.value).split(",");
+        for (var i = 0; i < majorChordProgressions.length; i++) {
+          var tr = "<tr>"
+          var output = convertProgToChords(majorChordProgressions[i], getChordsFromScale(asArray));
+          console.log(output);
+          tr += "<td>" + asArray[0] + "</td>" + "<td>" + output.join(", ") + "</td></tr>";
+          tbody.innerHTML += tr;
+          }
+    });
+}, false);
 
 
 /* loops through the chord progressions specified in the file
@@ -11,18 +36,5 @@ document.addEventListener('DOMContentLoaded', function() {
     var output = convertProgToChords(majorChordProgressions[i], cMajor)
     tr += "<td>" + cMajor[0] + "<td>" + "<td>" + output.join(", ") + "</td><tr>";
     tbody.innerHTML += tr;
-    }
-}, false);
-
-document.addEventListener('DOMContentLoaded', function() {
-  var dropdown = document.getElementById("dropdown");
-  for (var i = 0; i < majorScales.length; i++) {
-    var key = majorScales[i];
-    console.log(key);
-    var el = document.createElement("a");
-    el.textContent = (key.value)[0] + " major";
-    el.value = (key.value)[0] + " major";
-    el.className = "dropdown-item";
-    dropdown.appendChild(el);
     }
 }, false);
