3a7c91ffa1635ecc6aad44b38bb77baae146928b
Richard Hindes <rmhindes1@sheffield.ac.uk>
2019-07-14 10:37:01 +0100

    chord progression tool working but user can't specify key yet

--- scripts/progressions.js
+++ scripts/progressions.js
@@ -1,19 +1,28 @@
-function convertProgToChords(progression, chords) {
+var cMajor = ["C", "Dm", "Em", "F", "G", "Am", "Bdim"];
+
+/* given a chord progression in number form, returns that progression
+   in a given key
+
+   @params progression - an array of chord indices
+   @params key - an array of the diatonic chords in that key
+
+*/
+function convertProgToChords(progression, key) {
   var output = []
   for (var i = 0; i < progression.length; i++) {
-    output.push(chords[progression[i] - 1])
+    output.push(key[progression[i] - 1])
   }
   return output;
 }
 
+/* loops through the chord progressions specified in the file
+   initialise.js and outputs them in the given key in an HTML table */
 document.addEventListener('DOMContentLoaded', function() {
   var tbody = document.getElementById('tbody');
-  var chords = ["C", "Dm", "Em", "F", "G", "Am", "Bdim"];
-
-  for (var i = 0; i < majorChordProgressions; i++) {
-    tr += "<td>" + convertProgToChords(majorChordProgressions, chords) +"</td">
-
+  for (var i = 0; i < majorChordProgressions.length; i++) {
+    var tr = "<tr>"
+    var output = convertProgToChords(majorChordProgressions[i], cMajor)
+    tr += "<td>" + cMajor[0] + "<td>" + "<td>" + output.join(", ") + "</td><tr>";
     tbody.innerHTML += tr;
     }
-  }
 }, false);
